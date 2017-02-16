5abe140f1acebbff40bc4f193a1bd5e729b68e0a
DESKTOP-ASDLGRH\USER <jmy3028@gmail.com>
2017-02-16 21:22:50 +0900

    Fragments 생성 : MainFragment , ChattingFragment 생성 - 틀만 잡아놓은 상태 Chatting 레이아웃 약간 수정 : 리스트 뷰를 맨 마지막에 삽입(ListView를 item형식을 빼야 되나?)

---
+++ app/src/main/java/com/chattingapp/student/chattingapp/fragments/MainFragment.java
@@ -0,0 +1,24 @@
+package com.chattingapp.student.chattingapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by USER on 2017-02-16.
+ */
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
