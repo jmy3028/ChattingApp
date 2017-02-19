e005e6101eac5d100bb08ccf373bb40c04066e4d
DESKTOP-ASDLGRH\USER <jmy3028@gmail.com>
2017-02-19 11:55:42 +0900

    dialog 만들기 , 채팅창 수정

--- app/src/main/java/com/chattingapp/student/chattingapp/MainActivity.java
+++ app/src/main/java/com/chattingapp/student/chattingapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.chattingapp.student.chattingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agree);
+
+
+    }
+
+
+    public void onClick(View view) {
 
     }
 }
